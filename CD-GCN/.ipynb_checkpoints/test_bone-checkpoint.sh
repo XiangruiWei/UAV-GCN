8b8a96dd687c13db8f31ce41d1e7f4c2fddb37bd
XiangruiWei <2093653241@qq.com>
2024-11-12 10:47:45 +0800

    create cdgcn

---
+++ CD-GCN/.ipynb_checkpoints/test_bone-checkpoint.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=0 python main.py \
+--config ./config/test_bone.yaml \
+--phase test \
+--work-dir ./work_dir/cdgcn_test_bone \
+--weights ./weights/cdgcn_bone/cdgcn_bone-36-19314.pt
---
+++ CD-GCN/.ipynb_checkpoints/test_joint-checkpoint.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=0 python main.py \
+--config ./config/test_joint.yaml \
+--phase test \
+--work-dir ./work_dir/cdgcn_test_joint \
+--weights ./weights/cdgcn_joint/cdgcn_joint-34-18270.pt
